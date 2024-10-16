2bcb0f5b32de745dbbc1f27206921b3f9e7db906
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-16 15:46:59 +0530

    implemented line plot for data.txt . Also new idea to plot the data dynamically as it is generated from the get_date_temp function (not yet implemented)

--- web.py
+++ web.py
@@ -1,3 +1,8 @@
 import streamlit as st
-import pandas
+import pandas as pd
 import plotly.express as px
+
+df = pd.read_csv("data.txt")
+fig = px.line(df,x='date',y='temperature',labels={'x':'Date',
+                                                  'y':'Temperature'})
+st.plotly_chart(fig)
