7b9f72cff5b56f44db016ca46d1f8b456f381c80
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-16 15:51:32 +0530

    now shows labels

--- web.py
+++ web.py
@@ -3,6 +3,6 @@ import pandas as pd
 import plotly.express as px
 
 df = pd.read_csv("data.txt")
-fig = px.line(df,x='date',y='temperature',labels={'x':'Date',
+fig = px.line(x=df['date'],y=df['temperature'],labels={'x':'Date',
                                                   'y':'Temperature'})
 st.plotly_chart(fig)
